c7d5bb69d095500a622ebefc9af948f1745ea664
Alice <theliberalanimation@gmail.com>
2023-06-08 21:21:00 +0200

    Update finder.py

--- finder.py
+++ finder.py
@@ -6,7 +6,6 @@ mydico = {}
 with open('drivers.json', 'r', encoding="utf8") as f:
     json_data = json.load(f)
     
-
 terminate_functions = ["ZwTerminateProcess","NtTerminateProcess"]
 open_functions = ["ZwOpenProcess","NtOpenProcess"]
 
@@ -18,23 +17,21 @@ for driver in json_data:
         name_and_hash={}
 
         for elem in driver['KnownVulnerableSamples']:
-            if (any(func in elem['ImportedFunctions'] for func in terminate_functions) and any(func2 in elem['ImportedFunctions'] for func2 in open_functions)):
+
+            set_imported_functions = set(elem['ImportedFunctions'])
+            set_open = set(open_functions)
+            set_terminate = set(terminate_functions)
+
+            if((len(set_imported_functions.intersection(set_open)) > 0) and (len(set_imported_functions.intersection(set_terminate)) > 0)):
+
                 flag = 1
                 
-                try:
-                    md5list.append(elem["MD5"])
-                except:
-                    pass
-                
-                try:
-                    namelist.append(elem["Filename"])
-                except:
-                    try:
-                        namelist.append(elem["FileName"])
-                    except:
-                        namelist.append("no_name")       
+                for key, data in elem.items():
+                    if("md5" in key.lower()):
+                        md5list.append(elem[key])
+                    if("filename" in key.lower()):
+                        namelist.append(elem[key])
                     
-                
         if(flag):
             dico_driver_terminate[driver['Id']] = driver
             if(md5list):
@@ -47,15 +44,19 @@ for driver in json_data:
         continue
 
 
-#print(mydico["22aa985b-5fdb-4e38-9382-a496220c27ec"])
+
 for elem in mydico:
     print("\nID : "+elem)
     print("names : "+str(mydico[elem]["names"]))
     print("md5 : "+str(mydico[elem]["md5"]))
 
+#print(mydico["22aa985b-5fdb-4e38-9382-a496220c27ec"])
+
+"""
 unique_name = []
 for elem in mydico:
     for name in mydico[elem]["names"]:
         if name.lower() not in unique_name:
-            print(name.lower())
             unique_name.append(name.lower())
+            print(name)
+"""
